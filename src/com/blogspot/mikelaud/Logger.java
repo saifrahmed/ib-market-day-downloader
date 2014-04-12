9d1bca6b2d51296b447714f2e9275abd4d3f091d
Mike (Mykhailo) Oksenenko <mmx.mmx@gmail.com>
2014-04-11 22:02:36 -0400

    add logger

---
+++ src/com/blogspot/mikelaud/Logger.java
@@ -0,0 +1,18 @@
+package com.blogspot.mikelaud;
+
+public class Logger {
+
+	private static final java.util.logging.Logger LOGGER =
+		java.util.logging.Logger.getLogger(
+			Thread.currentThread().getStackTrace()[0].getClassName()
+		);
+	//
+	public static void logError(final String aMessage) { LOGGER.severe(aMessage); }
+	public static void logWarning(final String aMessage) { LOGGER.warning(aMessage); }
+	public static void logInfo(final String aMessage) { LOGGER.info(aMessage); }
+	public static void logDebug(final String aMessage) { LOGGER.config(aMessage); }
+	//
+	public static void print(final String aMessage) { System.out.print(aMessage); }
+	public static void println(final String aMessage) { System.out.println(aMessage); }
+	
+}
--- src/com/blogspot/mikelaud/Main.java
+++ src/com/blogspot/mikelaud/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Program program = new Program();
 		program.run();
-		System.out.println("Done.");
+		Logger.println("Done.");
 	}
 
 }
