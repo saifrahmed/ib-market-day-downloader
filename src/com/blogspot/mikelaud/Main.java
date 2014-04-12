f92efdf27dd157a9371c0bdb6cc0ff96f38c56f2
Mike (Mykhailo) Oksenenko <mmx.mmx@gmail.com>
2014-04-11 21:54:35 -0400

    add program

--- src/com/blogspot/mikelaud/Main.java
+++ src/com/blogspot/mikelaud/Main.java
@@ -3,6 +3,8 @@ package com.blogspot.mikelaud;
 public class Main {
 
 	public static void main(String[] args) {
+		Program program = new Program();
+		program.run();
 		System.out.println("Done.");
 	}
 
---
+++ src/com/blogspot/mikelaud/Program.java
@@ -0,0 +1,14 @@
+package com.blogspot.mikelaud;
+
+public class Program implements Runnable {
+
+	@Override
+	public void run() {
+		// void
+	}
+	
+	public Program() {
+		// void
+	}
+	
+}
